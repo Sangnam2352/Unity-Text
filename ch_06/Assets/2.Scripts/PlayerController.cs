d13209e9f69e6b9dde1992402093182ba00e3431
Sangnam2352 <vktnrl@gmail.com>
2025-04-30 13:59:19 +0900

    ch06_save

---
+++ ch_06/Assets/2.Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigd2D;
+    Animator animator;
+    float jumpForce = 350f;
+    public float WalkForce = 30f;
+    float maxWalkspeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rigd2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigd2D.AddForce(transform.up *  jumpForce);
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+
+        float speedX = Mathf.Abs(rigd2D.velocity.x);
+        if (speedX < maxWalkspeed)
+        {
+            rigd2D.AddForce(transform.right * key * WalkForce);
+        }
+
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+     animator.speed = speedX; 
+    }
+}
